8b6b080989a5904f4f1f086ca502b58e87d07651
lpenaud <l.penaud@zaclys.net>
2018-11-26 18:47:44 +0100

    Add test to calc_cle_hash

--- src/test_hachage.c
+++ src/test_hachage.c
@@ -3,13 +3,35 @@
 #include "test.h"
 #include "hachage.h"
 
+int calc_cle_hash(char str[])
+{
+    int i, cle = 0;
+    for (i = 0; str[i]; i++) {
+        cle *= 2;
+        cle += (int) str[i];
+    }
+    return cle % i;
+}
+
 int main(void)
 {
-    unsigned int len = 5;
+    const unsigned int len = 5;
     strhash_table * table = strhash_table_init(len);
+    char str[] = "Bonsoir";
 
-    if (!table) return 1;
+    if (!table) {
+        printf("Table n'a pas été créée\n");
+        return 1;
+    }
+
+    table = strhash_table_destroy(table);
+
+    // if (table) { // TODO: free(): invalid pointer
+    //     printf("La table n'a pas été détruite (%p)\n", table);
+    //     return 1;
+    // }
+
+    printf("%s -> cle : %d\n", str, calc_cle_hash(str));
 
     return 0;
 }
-
